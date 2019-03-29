88728d3b413ce6122f42e2834d834b3aee822a51
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-03-29 22:15:53 +0530

    Create perfectsqr.cpp

---
+++ perfectsqr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,i,j,c;
+    cin>>a>>b;;
+    i=sqrt(b);
+    j=sqrt(a);
+    c=i-j;
+    cout<<c;
+
+    return 0;
+}
